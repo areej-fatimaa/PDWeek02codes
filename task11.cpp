8ea1e1cdad7b35d839dcb754c5c6170284b216fd
areej-fatimaa <reejayfatima123@gmail.com>
2022-12-22 21:04:23 +0500

    my pd codes

---
+++ task11.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"          ____________________ "<<endl;
+cout<<"         /    |     |        \\"<<endl;
+cout<<"   _____/_____|_____|_________\\"<<endl;
+cout<<"  |_|       ,,          ,,     \\"<<endl;
+cout<<"  |________:  :________:  :_____\\"<<endl;
+cout<<"            ''          ''        "<<endl;           
+} 
